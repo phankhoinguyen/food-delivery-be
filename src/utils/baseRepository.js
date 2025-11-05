7bb7b7237a9dcde5142f6121c689133a9fe7e9de
phankhoinguyen <nguyentnphan123@gmail.com>
2025-11-05 23:10:41 +0700

    fix bug

--- src/utils/baseRepository.js
+++ src/utils/baseRepository.js
@@ -162,10 +162,10 @@ class BaseRepository {
                 const result = await this.model.findByIdAndDelete(id);
                 return !!result;
             } else {
-                await this.db.collection(this.collectionName).doc(id).delete();
                 const docRef = this.db.collection('cart').doc(id);
                 const docSnap = await docRef.get();
                 console.log('Before delete exists?', docSnap.exists);
+                await this.db.collection(this.collectionName).doc(id).delete();
                 console.log(`delete ${id} successfuly`)
                 return true;
             }
